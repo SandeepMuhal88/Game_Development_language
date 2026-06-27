282d7eea6cf48fd0e997285a40974e6bb3978048
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-06-27 21:44:45 +0530

    CHange in code

---
+++ basic_language/key_word.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// int main() {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
